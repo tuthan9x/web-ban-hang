b4724e2d57a968ecded4ee5727e9729b766e7b70
kumjShid4 <minhanpro123@gmail.com>
2018-05-27 15:30:02 +0700

    cấu hình mysql với database và web

---
+++ fn/db.js
@@ -0,0 +1,59 @@
+var mysql = require('mysql');
+
+exports.load = sql => {
+    return new Promise((resolve, reject) => {
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            port: 3306,
+            user: 'root',
+            password: 'root',
+            database: 'db_banhang'
+        });
+
+        connection.connect((err) => {
+            if (err) {
+                console.error('error connecting: ' + err.stack);
+                return;
+            }
+        });
+
+        connection.query(sql, (error, rows, fields) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(rows);
+            }
+
+            connection.end();
+        });
+    });
+}
+
+exports.save = sql => {
+    return new Promise((resolve, reject) => {
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            port: 3306,
+            user: 'root',
+            password: 'root',
+            database: 'db_banhang'
+        });
+
+        connection.connect((err) => {
+            if (err) {
+                console.error('error connecting: ' + err.stack);
+                return;
+            }
+        });
+
+        connection.query(sql, function (error, value) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(value);
+            }
+
+            connection.end();
+        });
+    });
+}
