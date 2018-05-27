4bca234b19a85a9507067e1a46e66797577719a1
kumjShid4 <minhanpro123@gmail.com>
2018-05-27 15:37:04 +0700

    app.js

---
+++ app.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+var express_handlebars_sections = require('express-handlebars-sections');
+var bodyParser = require('body-parser');
+var path = require('path');
+var wnumb = require('wnumb');
+
+var handleLayoutMDW = require('./middle-wares/handleLayout'),
+    handle404MDW = require('./middle-wares/handle404');
+
+var homeController = require('./controllers/homeController'),
+    categoryController = require('./controllers/categoryController'),
+    productController = require('./controllers/productController');
+
+var app = express();
+
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: 'views/_layouts/',
+    helpers: {
+        section: express_handlebars_sections(),
+        number_format: n => {
+            var nf = wnumb({
+                thousand: ','
+            });
+            return nf.to(n);
+        }
+    }
+}));
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(handleLayoutMDW);
+
+app.get('/', (req, res) => {
+    res.redirect('/home');
+});
+
+app.use('/home', homeController);
+app.use('/category', categoryController);
+app.use('/product', productController);
+
+app.use(handle404MDW);
+
+app.listen(3001, () => {
+    console.log('Site running on port 3000');
+});
