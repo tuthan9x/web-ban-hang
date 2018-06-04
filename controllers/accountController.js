51d08ca81f4a5bfd723c4a80c830e82ed85f597a
kumjShid4 <minhanpro123@gmail.com>
2018-06-04 23:05:07 +0700

    account controllers

---
+++ controllers/accountController.js
@@ -0,0 +1,65 @@
+var express = require('express'),
+    SHA256 = require('crypto-js/sha256'),
+    moment = require('moment');
+
+var accountRepo = require('../repos/accountRepo');
+var restrict = require('../middle-wares/restrict');
+
+var router = express.Router();
+
+router.get('/register', (req, res) => {
+    res.render('account/register');
+});
+
+router.post('/register', (req, res) => {
+
+    var dob = moment(req.body.dob, 'D/M/YYYY')
+        .format('YYYY-MM-DDTHH:mm');
+
+    var user = {
+        username: req.body.username,
+        password: SHA256(req.body.rawPWD).toString(),
+        name: req.body.name,
+        email: req.body.email,
+        dob: dob,
+        permission: 0
+    };
+
+    accountRepo.add(user).then(value => {
+        res.render('account/register');
+    });
+});
+
+router.get('/login', (req, res) => {
+    res.render('account/login');
+});
+
+router.post('/login', (req, res) => {
+    var user = {
+        username: req.body.username,
+        password: SHA256(req.body.rawPWD).toString()
+    };
+
+    accountRepo.login(user).then(rows => {
+        if (rows.length > 0) {
+            // user = rows[0];
+
+            req.session.isLogged = true;
+
+            res.redirect('/');
+
+        } else {
+            var vm = {
+                showError: true,
+                errorMsg: 'Login failed'
+            };
+            res.render('account/login', vm);
+        }
+    });
+});
+
+router.get('/profile', restrict, (req, res) => {
+    res.render('account/profile');
+});
+
+module.exports = router;
