b4a84de2b233b55b3095949026501313787ddfa7
kumjShid4 <minhanpro123@gmail.com>
2018-06-04 23:04:42 +0700

    tương tác với account trong db

---
+++ repos/accountRepo.js
@@ -0,0 +1,27 @@
+var db = require('../fn/db');
+
+exports.add = user => {
+    var sql = `insert into users(f_Username, f_Password, f_Name, f_Email, f_DOB, f_Permission) values('${user.username}', '${user.password}', '${user.name}', '${user.email}', '${user.dob}', ${user.permission})`;
+    return db.save(sql);
+}
+
+exports.login = user => {
+    var sql = `select * from users where f_Username = '${user.username}' and f_Password = '${user.password}'`;
+    return db.load(sql);
+}
+
+exports.single = (id) => {
+    return new Promise((resolve, reject) => {
+        var sql = `select * from users where f_Username = ${user.username}`;
+        db.load(sql).then(rows => {
+            if (rows.length === 0) {
+                resolve(null);
+            }
+            else {
+                resolve(rows[0]);
+            }
+        }).catch(err => {
+            reject(err);
+        });
+    });
+}
