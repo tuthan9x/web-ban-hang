a5728f8bb865eaa76211279f7136b9abab88140a
huynhquangdieu <huynhquangdieu0404@gmail.com.vn>
2018-06-26 20:03:38 +0700

    cap nhat middle-wares

--- middle-wares/handleLayout.js
+++ middle-wares/handleLayout.js
@@ -1,13 +1,20 @@
 var categoryRepo = require('../repos/categoryRepo');
 var brandRepo = require('../repos/brandRepo');
-
+var accountRepo=require('../repos/accountRepo');
 module.exports = (req, res, next) => {
+	if (req.session.isLogged === undefined) {
+		req.session.isLogged = false;
+	}
     var category = categoryRepo.loadAll();
     var brand = brandRepo.loadAll();
+    
     Promise.all([category, brand]).then(([categoryRows, brandRows]) =>{
         res.locals.layoutVM = {
             categories : categoryRows,
-            brands : brandRows
+            brands : brandRows,
+            isLogged:req.session.isLogged,
+            curUser:req.session.user
+
         };
         next();
     });
