1e354bef67cb3b2ecad8b8f8647459ffbbe0c081
huynhquangdieu <huynhquangdieu0404@gmail.com.vn>
2018-06-29 03:44:51 +0700

    lỗi

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -4,10 +4,6 @@ productRepo = require('../repos/productRepo');
 
 var router = express.Router();
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 77e7ee6278e8a4d8fe3ba524175ec1547fbef562
 router.get('/', (req, res) => {
 	var arr_p = [];
 	for (var i = 0; i < req.session.cart.length; i++) {
@@ -18,11 +14,7 @@ router.get('/', (req, res) => {
 
 	var items = [];
 	Promise.all(arr_p).then(result => {
-<<<<<<< HEAD
-		var sum = 0;
-=======
 		var sum = 0;	
->>>>>>> 77e7ee6278e8a4d8fe3ba524175ec1547fbef562
 		for (var i = result.length - 1; i >= 0; i--) {
 		var pro = result[i];
 		var item = {
@@ -48,10 +40,6 @@ router.post('/add', (req, res) => {
 		ProId: req.body.proID,
 		Quantity: +req.body.quantity
 	};
-<<<<<<< HEAD
-	
-=======
->>>>>>> 77e7ee6278e8a4d8fe3ba524175ec1547fbef562
 	cartRepo.add(req.session.cart, item);
 	res.redirect('/checkout');
 });
