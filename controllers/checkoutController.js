187f31fbc76b98ea242ea45cef8054012c43560e
Thanh Truc <duy.anh01997@gmail.com>
2018-06-29 07:33:38 +0700

    chỉnh sửa để thêm danh sách đơn hàng

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cartRepo = require('../repos/cartRepo'),
-productRepo = require('../repos/productRepo');
-
+productRepo = require('../repos/productRepo'),
+orderRepo = require('../repos/orderRepo');
 var router = express.Router();
 
 router.get('/', (req, res) => {
@@ -55,7 +55,7 @@ router.post('/subOne', (req, res) => {
 	var item = {
 		ProId: req.body.proID,
 		Quantity: -1
-	};
+};
 	cartRepo.add(req.session.cart, item);
 	res.redirect('/checkout');
 });
@@ -63,5 +63,50 @@ router.post('/remove', (req, res) => {
 	cartRepo.remove(req.session.cart, req.body.ProId);
 	res.redirect(req.headers.referer);
 });
-    
+router.get('/order', (req, res) => {
+	res.render('checkout/order');
+});
+
+router.post('/billpay', (req, res) => {
+    var date = new Date();
+    var bill = {
+        name: req.session.user.Name,
+        email: req.session.user.Email,
+        address: req.body.addresss,
+        phone: req.body.phone,
+        total: req.body.sum,
+	}
+
+    var arr_p = [];
+    var itemsquanti = [];
+   
+    for (var i = 0; i < req.session.cart.length; i++) {
+        var quan = req.session.cart[i].Quantity;
+        itemsquanti.push(quan);
+    }
+
+    orderRepo.addbill(bill).then(row => {
+		orderRepo.getbillid(bill.phone).then(rowid => {
+            var billid = rowid[0].ID;
+            Promise.all([arr_p, itemsquanti]).then(([result, qui]) => {
+                for (var i = result.length - 1; i >= 0; i--) {
+                    var pro = result[i][0];
+                    console.log(result.length);
+                    var detail = {
+                        orderId: billid,
+                        proId: pro.ProID,
+                        quantity: qui[i],
+                        price: pro.Price,
+                        sum: pro.Price * qui[i]
+                    }
+                    console.log(detail);
+                    orderRepo.addbilldetail(detail);
+				}
+			});
+        });
+		console.log(req.body);
+    });
+    orderRepo.removeall(req.session.cart);
+    res.redirect('order');
+});
 module.exports = router;
--- repos/orderRepo.js
+++ repos/orderRepo.js
@@ -20,15 +20,28 @@ exports.single = (OrderId) => {
         });
     });
 }
- exports.addorder =(Order)=>{
-     var sql = `INSERT INTO orders VALUES('${Order.Id}','NOW()','${Order.userId}','${Order.address}','${Order.total}')`;
+ exports.addbill =(Order)=>{
+     var sql = `INSERT INTO orders(OrderDate,UserID,Address,Total,Phone) VALUES('NOW()','${Order.userId}','${Order.address}','${Order.total}','${Order.phone}')`;
      return db.save(sql);
 
 }
 
-exports.addorderdetail = detail => {
-    var sql = `INSERT INTO orderdetails VALUES('${detail.Id}', '${detail.OrderId}','${detail.proId}', '${ct.quantity}', '${ct.price}','${detail.sum}')`;
+exports.addbilldetail = detail => {
+    var sql = `INSERT INTO orderdetails(OrderID,ProID,Quantity,Price,Amount) VALUES('${detail.OrderId}','${detail.proId}', '${detail.quantity}', '${detail.price}','${detail.sum}')`;
     return db.save(sql);
+}
+
+exports.getbillid=email=>{
+    var sql=`SELECT details.OrderID as ID FROM details where Phone='${phone}' `;
+    return db.save(sql);
+
+}
+
+exports.removeall = (cart) => {
+    for (var i = cart.length - 1; i >= 0; i--) {
+            cart.splice(i, 1);
+    }
+    return;
 }
 
 exports.add = (cart, item) => {
